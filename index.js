4b6478b646e1ae94d1329bc36885af203365a323
neeravbaraha <neeravbaraha07@gmail.com>
2025-02-28 17:48:38 +0545

    Time SOlution

---
+++ index.js
@@ -0,0 +1,10 @@
+let date= new Date();
+
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toJSON());
+console.log(date.toLocaleString());
+console.log(typeof date);
+
+let newDate=new Date (2024,0,3);
+console.log(newDate.toString());
